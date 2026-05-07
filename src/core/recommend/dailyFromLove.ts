434930712c52a0cb6b178a15268a54f2f19d60d0
zhanghan <3453103310@qq.com>
2026-05-07 13:39:59 +0800

    优化歌曲

--- src/core/recommend/dailyFromLove.ts
+++ src/core/recommend/dailyFromLove.ts
@@ -1,11 +1,12 @@
 import { createUserList, overwriteListMusics } from '@/core/list'
 import listState from '@/store/list/state'
 import { getDailyRecommendDate, setDailyRecommendDate } from '@/utils/data'
-import { FIXED_DAILY_RECOMMEND_LIST } from './dailyRecommendList'
+import { DAILY_RECOMMEND_DATA_REVISION, FIXED_DAILY_RECOMMEND_LIST } from './dailyRecommendList'
 
 const DAILY_RECOMMEND_LIST_ID = 'daily_recommend'
 const DAILY_RECOMMEND_LIST_NAME = '每日推荐'
-const DAILY_RECOMMEND_DATA_VERSION = '2026-04-28-v2'
+/** 与内置歌单数据绑定；改歌单请改 dailyRecommendList.ts 里的 DAILY_RECOMMEND_DATA_REVISION */
+const DAILY_RECOMMEND_DATA_VERSION = `embedded-v${DAILY_RECOMMEND_DATA_REVISION}`
 
 const ensureRecommendList = async() => {
   if (listState.userList.some(item => item.id == DAILY_RECOMMEND_LIST_ID)) return
